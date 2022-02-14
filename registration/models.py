3aa2e9cd36720278c28df779771609ab2dd9b915
DozerTrashpanda <99625007+DozerTrashpanda@users.noreply.github.com>
2022-02-13 20:38:08 -0500

    Update models.py
    Fix dealer checkout when assistant who have already paid are attached

--- registration/models.py
+++ registration/models.py
@@ -578,9 +578,9 @@ class Dealer(models.Model):
             return "%s %s" % (self.attendee.firstName, self.attendee.lastName)
         return "<Dealer(orphan)>"
 
-    def getPartnerCount(self):
-        partnercount = self.dealerasst_set.count()
-        return partnercount
+    def getUnpaidPartnerCount(self):
+        partnercount = dealer.dealerasst_set.all().filter(paid=False).count()
+        return unpaidpartnercount
 
     def paidTotal(self):
         total = 0
